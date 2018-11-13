267459e7d2bfe75cca28fea984177c50f2ac397b
Markus Wolf <mail@markus-wolf.de>
2018-11-13 15:46:26 +0100

    chore: setup jest-puppeteer on linux

--- jest.integration.js
+++ jest.integration.js
@@ -1,8 +1,8 @@
-const ts = require("ts-jest");
-const puppeteer = require("jest-puppeteer/jest-preset");
+const ts = require('ts-jest');
+const puppeteer = require('jest-puppeteer/jest-preset');
 
 module.exports = {
   ...ts.jestPreset,
   ...puppeteer,
-  testMatch: ["<rootDir>/test/*.test.ts"]
+  testMatch: ['<rootDir>/test/*.test.ts']
 };
